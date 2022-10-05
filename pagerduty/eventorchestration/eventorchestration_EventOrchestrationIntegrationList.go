30625de473aa4d41b5b03192bf0a6cfb63146da7
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-10-05 00:16:37 +0000

    chore(release): v3.0.2

--- pagerduty/eventorchestration/eventorchestration_EventOrchestrationIntegrationList.go
+++ pagerduty/eventorchestration/eventorchestration_EventOrchestrationIntegrationList.go
@@ -2,9 +2,9 @@ package eventorchestration
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-pagerduty-go/pagerduty/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-pagerduty-go/pagerduty/v3/jsii"
 
-	"github.com/hashicorp/cdktf-provider-pagerduty-go/pagerduty/v3/eventorchestration/internal"
+	"github.com/cdktf/cdktf-provider-pagerduty-go/pagerduty/v3/eventorchestration/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
--- pagerduty/serviceeventrule/serviceeventrule_ServiceEventRuleActionsPriorityOutputReference.go
+++ pagerduty/serviceeventrule/serviceeventrule_ServiceEventRuleActionsPriorityOutputReference.go
@@ -2,9 +2,9 @@ package serviceeventrule
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-pagerduty-go/pagerduty/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-pagerduty-go/pagerduty/v3/jsii"
 
-	"github.com/hashicorp/cdktf-provider-pagerduty-go/pagerduty/v3/serviceeventrule/internal"
+	"github.com/cdktf/cdktf-provider-pagerduty-go/pagerduty/v3/serviceeventrule/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
