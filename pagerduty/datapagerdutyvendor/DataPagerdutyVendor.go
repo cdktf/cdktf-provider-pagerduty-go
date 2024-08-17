174d25636f07019794cf9a60ae0616f128d45417
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2024-08-17 04:00:22 +0000

    chore(release): v13.11.4

--- pagerduty/datapagerdutyvendor/DataPagerdutyVendor.go
+++ pagerduty/datapagerdutyvendor/DataPagerdutyVendor.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
-// Represents a {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.15.3/docs/data-sources/vendor pagerduty_vendor}.
+// Represents a {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.15.4/docs/data-sources/vendor pagerduty_vendor}.
 type DataPagerdutyVendor interface {
 	cdktf.TerraformDataSource
 	// Experimental.
@@ -299,7 +299,7 @@ func (j *jsiiProxy_DataPagerdutyVendor) Type() *string {
 }
 
 
-// Create a new {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.15.3/docs/data-sources/vendor pagerduty_vendor} Data Source.
+// Create a new {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.15.4/docs/data-sources/vendor pagerduty_vendor} Data Source.
 func NewDataPagerdutyVendor(scope constructs.Construct, id *string, config *DataPagerdutyVendorConfig) DataPagerdutyVendor {
 	_init_.Initialize()
 
@@ -317,7 +317,7 @@ func NewDataPagerdutyVendor(scope constructs.Construct, id *string, config *Data
 	return &j
 }
 
-// Create a new {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.15.3/docs/data-sources/vendor pagerduty_vendor} Data Source.
+// Create a new {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.15.4/docs/data-sources/vendor pagerduty_vendor} Data Source.
 func NewDataPagerdutyVendor_Override(d DataPagerdutyVendor, scope constructs.Construct, id *string, config *DataPagerdutyVendorConfig) {
 	_init_.Initialize()
 
