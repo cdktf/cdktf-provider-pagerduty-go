fb52ce9b6b5d8771e0c440c7dc2ae43c028c6227
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2024-11-09 04:01:14 +0000

    chore(release): v13.13.1

--- pagerduty/eventorchestrationservice/EventOrchestrationService.go
+++ pagerduty/eventorchestrationservice/EventOrchestrationService.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
-// Represents a {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.17.0/docs/resources/event_orchestration_service pagerduty_event_orchestration_service}.
+// Represents a {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.17.1/docs/resources/event_orchestration_service pagerduty_event_orchestration_service}.
 type EventOrchestrationService interface {
 	cdktf.TerraformResource
 	CatchAll() EventOrchestrationServiceCatchAllOutputReference
@@ -403,7 +403,7 @@ func (j *jsiiProxy_EventOrchestrationService) TerraformResourceType() *string {
 }
 
 
-// Create a new {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.17.0/docs/resources/event_orchestration_service pagerduty_event_orchestration_service} Resource.
+// Create a new {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.17.1/docs/resources/event_orchestration_service pagerduty_event_orchestration_service} Resource.
 func NewEventOrchestrationService(scope constructs.Construct, id *string, config *EventOrchestrationServiceConfig) EventOrchestrationService {
 	_init_.Initialize()
 
@@ -421,7 +421,7 @@ func NewEventOrchestrationService(scope constructs.Construct, id *string, config
 	return &j
 }
 
-// Create a new {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.17.0/docs/resources/event_orchestration_service pagerduty_event_orchestration_service} Resource.
+// Create a new {@link https://registry.terraform.io/providers/pagerduty/pagerduty/3.17.1/docs/resources/event_orchestration_service pagerduty_event_orchestration_service} Resource.
 func NewEventOrchestrationService_Override(e EventOrchestrationService, scope constructs.Construct, id *string, config *EventOrchestrationServiceConfig) {
 	_init_.Initialize()
 
